0cbef952290129d1892a6d70994d33646b0c4d12
xkhadmy <dimaadboos@gmail.com>
2025-04-13 19:18:33 +0200

    EMAIL_HOST

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,6 +17,13 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+EMAIL_HOST = "smtp.gmail.com" 
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'cauto1919@gmail.com'
+EMAIL_HOST_PASSWORD = 'rlnkhsifemamcqzk'
+EMAIL_USE_TLS = True
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
